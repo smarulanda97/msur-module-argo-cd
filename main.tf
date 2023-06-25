4fbc65d07c40db9c0153fb61d4c1b16bbf454b02
Santiago Marulanda <santiagomm1997@gmail.com>
2023-06-26 01:22:31 +0200

    update api authentication

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
     args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
   }
